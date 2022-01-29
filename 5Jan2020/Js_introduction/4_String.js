f79c895b4dbb8360889c649e12591c945a8303c0
vijay0095 <vijayvishwakarma0095@gmail.com>
2022-01-29 15:06:57 +0530

    commit

---
+++ 5Jan2020/Js_introduction/4_String.js
@@ -0,0 +1,50 @@
+// //String is Sequence of Characters
+// let str = 'Vijayvishwakarma'
+// console.log(str)
+
+// //length property
+// console.log(str.length)// space will also count
+
+// // string method
+// // 1.Extraction method
+
+// // Slice method----> slice (starting indice, end +1)
+// let slicestr = str.slice(0,5)
+// console.log(slicestr)
+
+// //2.substr method
+// // substr( startind index, lengthupto)
+// let substr = str.substr(2,3)
+// console.log(substr)
+
+// //Replacing string content 
+// let sayHello = 'Hello Vijay'
+// console.log(sayHello)
+
+// // replace(which want to replce , with what)
+// let sayBye = sayHello.replace('Hello','Bye')
+// console.log(sayBye)
+
+// Uppercase and Lowercase method
+
+// let text1 = 'Hello World'
+// //to upper case
+// let UC = text1.toUpperCase(text1)
+// console.log(UC)
+// //to Lower Case
+// let LC =  text1.toLowerCase(text1)
+// console.log(LC)
+
+// // concatenation method
+// let firststring = 'Hello'
+// let Secondstring = 'Vijay'
+
+// let concated = firststring.concat(' Bhai ',Secondstring)
+// console.log(concated)
+
+// Trim method used to remove white spaces
+let text = '         Hello Vijay         '
+console.log(text)
+
+let trimed = text.trim()
+console.log(trimed)
