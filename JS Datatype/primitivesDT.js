7243e3a174334e93e64aea3cc5f70aed89e0a9f8
vijay0095 <vijayvishwakarma0095@gmail.com>
2022-02-04 00:16:48 +0530

    Added

---
+++ JS Datatype/primitivesDT.js
@@ -0,0 +1,38 @@
+// //primitve data type
+// // numbers//string//boolean//undifiend//null//bigint//symbol
+
+// //Number Datatype
+// let a = 12
+// console.log(a, typeof(a))
+
+// let b = -12
+// console.log(b, typeof(b))
+
+// let c = 12.265
+// console.log(c, typeof(c))
+
+// //String
+// let d = 'a'
+// console.log(d, typeof(d))
+
+// let e = 'String'
+// console.log(e, typeof(e))
+
+// //undefined data type
+// let f = undefined
+// console.log(f, typeof(f))
+
+// //object Data type
+// let g = null
+// console.log(g, typeof(g))//but object type is a refference type not priimitive... but putted in this?
+// //all object driveds form null value
+
+//Access by value ----> value store in Stack
+var firstPerson = 'vijay'
+
+var secondPerson = firstPerson
+
+var firstPerson = 'vishwakarma'
+
+console.log(firstPerson) //vishwakarma
+console.log(secondPerson) // vijay
---
+++ JS Datatype/refferencDT.js
@@ -0,0 +1,32 @@
+//Reffenerce Data type ...
+//object //function //array
+//techinically.. all are objects
+
+// //objest definds with their key
+// let person= {
+//     name : 'vijay',
+//     age : 26
+// }
+// console.log(person, typeof(person))
+
+// //array
+// let arr = [1, 2 ,'a','abc',null, undefined]
+// console.log(arr,typeof(arr))//object...not array...that is array data type is not present ...
+// //object some what its modified to used as array 
+
+// //function
+// function great(){
+//     console.log('HELLO')
+// }
+// console.log(great,typeof(great))
+
+let firstPerson = {
+    name : 'vijay',
+    age : 26
+}
+
+let secondPerson = firstPerson
+
+secondPerson.name = 'vishwakarma'
+console.log(firstPerson) //vishwakarma
+console.log(secondPerson) //vishwakarma
